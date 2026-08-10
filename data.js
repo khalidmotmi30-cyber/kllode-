1d63ec9deea503bd75f720aae778f853c29a4abd
khalidmotmi30-cyber <khalidmotmi30@gmail.com>
2026-08-10 08:35:24 +0300

    Add configurable operations units

---
+++ data.js
@@ -0,0 +1,17 @@
+window.DEFAULT_UNITS = [
+  { name: 'قيادة', code: 'E-000' },
+  { name: 'إشراف عام', code: 'E-000' },
+  { name: 'مشرف ميداني', code: 'E-000' },
+  { name: 'العمليات', code: 'E-000' },
+  { name: 'نائب العمليات', code: 'E-000' },
+  { name: 'سين 1', code: '' },
+  { name: 'سين', code: '' },
+  { name: 'باء 1', code: '' },
+  { name: 'باء', code: '' },
+  { name: 'جيم 1', code: '' },
+  { name: 'جيم 2', code: 'E-000' },
+  { name: 'دعم', code: 'E-000' },
+  { name: 'الميناء', code: 'E-000' },
+  { name: 'وحدات البحث و الإنقاذ', code: 'E-000 E-000' },
+  { name: 'تسجيل خروج', code: 'E-000' }
+];
